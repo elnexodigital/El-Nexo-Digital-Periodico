c585721b7e4561e21e6dade39149379e8eb85e3d
Leo castrillo <castrilloleo@gmail.com>
2025-11-25 21:01:23 -0300

    Música nueva

--- src/data/broadcasts.ts
+++ src/data/broadcasts.ts
@@ -4,12 +4,12 @@ import type { NewsBroadcast } from '../types.ts';
 export const NEWS_BROADCASTS: Record<number, NewsBroadcast> = {
   10: {
     id: 'news10',
-    url: 'https://res.cloudinary.com/dgvkbrgre/video/upload/q_auto:good/v1758255289/noti19_gabriel_esjdge.mp3',
+    url: 'https://res.cloudinary.com/dnauavz56/video/upload/v1764090883/Bataille_Gasto_El_Sol_Erotismo_Fascismo_bxzuy6.mp3',
     description: 'AGENDA CULTURAL CON GABRIEL CALLUM.',
   },
   12: {
     id: 'news12',
-    url: 'https://res.cloudinary.com/dgvkbrgre/video/upload/q_auto:good/v1758254890/noti_19_grace_patkec.mp3',
+    url: 'https://res.cloudinary.com/dnauavz56/video/upload/v1764090883/Bataille_Gasto_El_Sol_Erotismo_Fascismo_bxzuy6.mp3',
     description: 'MICRO TEMÁTICO CON GRACIELA AQUELARRE.',
   },
 };
