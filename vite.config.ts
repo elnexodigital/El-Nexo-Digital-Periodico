71c604836a0f439edc15fe0f8118fe6959998e6f
Leo castrillo <castrilloleo@gmail.com>
2026-03-08 15:24:21 -0300

    actualización biblioteca

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
     define: {
       'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
     },
